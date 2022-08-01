b2792f19ee5954ecc7b3f2e671580ef86b9d3af3
lagiewnik <lagiewnik@gmail.com>
2022-08-01 21:26:26 +0200

    second  lesson

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,4 +1,4 @@
-describe('empty spec', () => {
+describe("empty spec", () => {
   beforeEach(()=>{
     cy.visit('http://localhost:3000')
   })
---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,20 @@
+describe("Newslatter Subsribe Form", ()=>{
+    beforeEach(()=>{
+        cy.visit("/")
+    })
+
+    it('Allow users to subscribe to the email list', () => {
+        const email = "ziom@ziom.pl"
+        cy.getByData("email-input").type(email)
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("exist").contains(email)
+    });
+
+    it('Doesnt allow an invalid email adress', () => {
+        const invalidEmail = "invalidEmail"
+        cy.getByData("email-input").type(invalidEmail)
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("not.exist")
+        
+    });
+})
